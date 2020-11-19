b6eec7f0e7fdae27814e5a8501c03755282b1aee
System Ato <atosystem@hotmail.com>
2020-11-19 21:11:36 +0800

    add run.sh

---
+++ PA2/b07901033_pa2_edit/run.sh
@@ -0,0 +1,17 @@
+./bin/mps my_in/900_0.in my_out/900_0.out
+./bin/mps my_in/900_1.in my_out/900_1.out
+./bin/mps my_in/900_2.in my_out/900_2.out
+./bin/mps my_in/900_3.in my_out/900_3.out
+./bin/mps my_in/900_4.in my_out/900_4.out
+
+./bin/mps my_in/9000_0.in my_out/9000_0.out
+./bin/mps my_in/9000_1.in my_out/9000_1.out
+./bin/mps my_in/9000_2.in my_out/9000_2.out
+./bin/mps my_in/9000_3.in my_out/9000_3.out
+./bin/mps my_in/9000_4.in my_out/9000_4.out
+
+./bin/mps my_in/90000_0.in my_out/90000_0.out
+./bin/mps my_in/90000_1.in my_out/90000_1.out
+./bin/mps my_in/90000_2.in my_out/90000_2.out
+./bin/mps my_in/90000_3.in my_out/90000_3.out
+./bin/mps my_in/90000_4.in my_out/90000_4.out
